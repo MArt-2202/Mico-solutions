c06e6905ead5a215bccffebe12768f8f15f4db54
Artem <mart_22@ukr.net>
2022-10-13 14:52:54 +0300

    Our work list styles in nodeStyles.js

--- src/js/modules/nodeStyles.js
+++ src/js/modules/nodeStyles.js
@@ -6,4 +6,19 @@ export default function nodeStyles() {
 	} else {
 		document.body.classList.add('inner');
 	}
+
+	if (document.querySelector('#our-work-list a')) {
+		document.querySelectorAll('#our-work-list a').forEach((el) => {
+			const itemHref = el.getAttribute('href');
+
+			if (itemHref !== '#' && itemHref !== 'javascript:void(0)') {
+				if (el.querySelector('span')) {
+					const itemTitle = document.createElement('strong');
+
+					itemTitle.textContent = itemHref;
+					el.querySelector('span').after(itemTitle);
+				}
+			}
+		});
+	}
 }

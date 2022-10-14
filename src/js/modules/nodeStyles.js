85d743e69149598c6bd2ab9352d87d7f84ae33a3
Artem <mart_22@ukr.net>
2022-10-14 23:15:48 +0300

    Edit nodeStyles.js

--- src/js/modules/nodeStyles.js
+++ src/js/modules/nodeStyles.js
@@ -21,4 +21,46 @@ export default function nodeStyles() {
 			}
 		});
 	}
+
+	if (
+		document.querySelector('#request-form__phone') &&
+		document.querySelector('#request-form__site') &&
+		document.querySelector('#request-form__submit')
+	) {
+		const formPhone = document.querySelector('#request-form__phone'),
+			formSite = document.querySelector('#request-form__site'),
+			formSubmit = document.querySelector('#request-form__submit');
+
+		if (document.querySelector('#request-form')) {
+			document.querySelector('#request-form').addEventListener('input', function (e) {
+				if (
+					e.target.closest('input') &&
+					e.target.closest('input').value === '' &&
+					!e.target.closest('input').classList.contains('required')
+				) {
+					e.target.closest('input').classList.add('required');
+				} else {
+					e.target.closest('input').classList.remove('required');
+				}
+			});
+		}
+
+		formSubmit.addEventListener('click', function () {
+			if (formPhone.value === '') {
+				formPhone.classList.add('required');
+			} else {
+				formPhone.classList.remove('required');
+			}
+			if (formSite.value === '') {
+				formSite.classList.add('required');
+			} else {
+				formSite.classList.remove('required');
+			}
+			if (formPhone.value !== '' && formSite.value !== '') {
+				if (document.querySelector('#request')) {
+					document.querySelector('#request').classList.add('success');
+				}
+			}
+		});
+	}
 }

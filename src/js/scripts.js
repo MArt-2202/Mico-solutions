cd15bb9c7567e2ced836aa5ac07a53f9178bfc05
Artem <mart_22@ukr.net>
2022-10-20 15:27:46 +0300

    Import, init scrollAnimateNodes in scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,6 +5,7 @@ import nodeStyles from './modules/nodeStyles';
 import blockPosition from './modules/blockPosition';
 import tableWrapper from './modules/tableWrapper';
 import toggleContent from './modules/toggleContent';
+import scrollAnimateNodes from './modules/scrollAnimateNodes';
 
 if ('ontouchstart' in document.documentElement) {
 	document.body.classList.add('touchdevice');
@@ -27,6 +28,22 @@ if (isMobile()) {
 document.addEventListener('DOMContentLoaded', () => {
 	tableWrapper();
 	toggleContent();
+	scrollAnimateNodes({
+		node: 'h1, h2, h3, h4',
+		animatedClass: 'flipInX',
+	});
+	scrollAnimateNodes({
+		node: '.request',
+		animatedClass: 'flipInY',
+	});
+	scrollAnimateNodes({
+		node: '.our-advantages__list li, .our-works__list li, .disqus__list li, .disqus__form div',
+		animatedClass: 'slideInUp',
+	});
+	scrollAnimateNodes({
+		node: '.services-list-1 li, .services-list-2 li, .services-list-3 li, .services__img, .info__img picture, .info p, .disqus__content strong',
+		animatedClass: 'fadeIn',
+	});
 }); // END READY
 
 window.addEventListener('resize', () => {

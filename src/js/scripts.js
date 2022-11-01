3bddc621af76e0dab423284069cff623ebd463d2
Artem <mart_22@ukr.net>
2022-11-01 16:33:57 +0200

    Import, init blocksStyles, add new nodes for scrollAnimateNodes in scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,6 +6,7 @@ import blockPosition from './modules/blockPosition';
 import tableWrapper from './modules/tableWrapper';
 import toggleContent from './modules/toggleContent';
 import scrollAnimateNodes from './modules/scrollAnimateNodes';
+import blocksStyles from './modules/blocksStyles';
 
 if ('ontouchstart' in document.documentElement) {
 	document.body.classList.add('touchdevice');
@@ -28,6 +29,7 @@ if (isMobile()) {
 document.addEventListener('DOMContentLoaded', () => {
 	tableWrapper();
 	toggleContent();
+	blocksStyles();
 	scrollAnimateNodes({
 		node: 'h1, h2, h3, h4',
 		animatedClass: 'flipInX',
@@ -41,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		animatedClass: 'slideInUp',
 	});
 	scrollAnimateNodes({
-		node: '.top-section .btn, .s_ocial li, .services-list-1 li, .services-list-2 li, .services-list-3 li, .services__img, .info__img picture, .info p, .disqus__content strong',
+		node: '.top-section .btn, .s_ocial li, .contacts__list li, .services-list-1 li, .services-list-2 li, .services-list-3 li, .services__img, .info__img picture, .info p, .disqus__content strong',
 		animatedClass: 'fadeIn',
 	});
 }); // END READY

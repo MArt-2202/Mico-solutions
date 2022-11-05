06a1cedf261e7053a8fb1567d756d490995925d8
Artem <mart_22@ukr.net>
2022-11-05 20:59:26 +0200

    Edit nodeStyles.js

--- src/js/modules/nodeStyles.js
+++ src/js/modules/nodeStyles.js
@@ -12,22 +12,25 @@ export default function nodeStyles() {
 		document.querySelector('.top-section-style-2 .content-2__aside')
 	) {
 		const main = document.querySelector('.top-section-style-2 .content-2__main'),
-			aside = document.querySelector('.top-section-style-2 .content-2__aside');
+			aside = document.querySelector('.top-section-style-2 .content-2__aside'),
+			addBlock1 = document.querySelector('.top-section-style-2 .content-2__aside .request'),
+			addBlock2 = document.querySelector(
+				'.top-section-style-2 .content-2__aside .request__content'
+			),
+			addBlock3 = document.querySelector(
+				'.top-section-style-2 .content-2__aside .request__front'
+			);
 		if (window.matchMedia('(min-width: 1025px)').matches) {
 			aside.style.minHeight = `${main.offsetHeight}px`;
-			document.querySelector(
-				'.top-section-style-2 .content-2__aside .request'
-			).style.minHeight = `${main.offsetHeight}px`;
-			document.querySelector(
-				'.top-section-style-2 .content-2__aside .request__content'
-			).style.minHeight = `${main.offsetHeight}px`;
-
-			document.querySelector(
-				'.top-section-style-2 .content-2__aside .request__front'
-			).style.minHeight = `${main.offsetHeight}px`;
+			addBlock1.style.minHeight = `${main.offsetHeight}px`;
+			addBlock2.style.minHeight = `${main.offsetHeight}px`;
+			addBlock3.style.minHeight = `${main.offsetHeight}px`;
 		}
 		if (window.matchMedia('(max-width: 1024px)').matches) {
 			aside.style.minHeight = '';
+			addBlock1.style.minHeight = '';
+			addBlock2.style.minHeight = '';
+			addBlock3.style.minHeight = '';
 		}
 	}
 
